1819ccf1269fef07dd1e5fe224d4705ebda8d8d4
seonokmin <dnsmsdkdl@gmail.com>
2024-06-16 21:10:40 +0900

    검색 히스토리 css 변경

--- src/components/SideHead.js
+++ src/components/SideHead.js
@@ -1,7 +1,7 @@
 /** @format */
 
-import Chip from "@mui/material/Chip"
-import Stack from "@mui/material/Stack"
+import { useState } from "react"
+import { Stack, Chip, Box } from "@mui/material"
 import logo from "../assets/logo.png"
 import styled from "styled-components"
 import SearchBar from "./Search/SearchBar.js"
@@ -11,14 +11,14 @@ const Header = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-	
+
   & img {
     padding-top: 5vh;
     width: 80%;
   }
 
   @media (max-width: 768px) {
-		align-items: flex-start;
+    align-items: flex-start;
     & img {
       padding-left: 8%;
       width: 50%;
@@ -26,17 +26,52 @@ const Header = styled.div`
   }
 `
 
+const ChipWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`
+
 function SideHead({ setAddress }) {
+  const [open, setOpen] = useState(false)
+
   const handleClick = () => {
-    console.info("You clicked the Chip.")
+    console.log("Chip clicked")
+    setOpen(!open)
   }
 
   return (
     <Header>
       <img src={logo} className="logo" alt="logo" />
       <SearchBar setAddress={setAddress} />
-      <Stack direction="row" spacing={1} justifyContent="flex-end">
-        <Chip label="검색히스토리" color="primary" onClick={handleClick} />
+      <Stack
+        direction="row"
+        spacing={1}
+        justifyContent="flex-end"
+        alignItems="flex-start"
+        sx={{ width: "100%", height: "5rem", paddingRight: "2rem" }}
+      >
+        <ChipWrapper>
+          <Chip label="검색히스토리" color="primary" onClick={handleClick} />
+          {open && (
+            <Box
+              sx={{
+                width: 300,
+                height: 200,
+                backgroundColor: "#fff",
+                position: "absolute",
+                zIndex:10,
+                top: "30px", 
+                right: 0,
+                border: "1px solid grey",
+                borderRadius: "4px",
+                boxShadow: 3,
+                p: 2,
+              }}
+            >
+              검색 히스토리 내용
+            </Box>
+          )}
+        </ChipWrapper>
       </Stack>
     </Header>
   )
