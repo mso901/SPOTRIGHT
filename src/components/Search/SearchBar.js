7b04ff250824e01e49c3aab070df9a30b2023ff3
seonokmin <dnsmsdkdl@gmail.com>
2024-06-16 19:50:23 +0900

    온보딩 라이브러리, 검색 히스토리 삭제

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,74 +1,16 @@
 /** @format */
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DaumPostcode from "react-daum-postcode";
-import styled from "styled-components";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import Modal from "@mui/material/Modal";
 import SearchIcon from "@mui/icons-material/Search";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 
 
-const { kakao } = window;
-
-const InputContainer = styled.div`
-	width: 100%;
-	text-align: center;
-`;
-
-const Input = styled.input`
-	margin: 5vh auto 5vh;
-	width: 85%;
-	height: 4vh;
-	border-radius: 2.5rem;
-	border: 3px solid #004aad;
-`;
-const RemoveIcon = styled.span``;
-
-// function SearchBar({ onAddAddress, onInputClick }) {
-// 	const [address, setAddress] = useState("");
-
-// 	const handleAddress = (e) => {
-// 		setAddress(e.target.value);
-// 	};
-
-// 	// address 추가 후 입력 필드를 초기화
-// 	const handleEnter = (e) => {
-// 		//Enter 키가 눌렸는지 확인
-// 		if (address && e.keyCode === 13) {
-// 			onAddAddress(address);
-// 			setAddress("");
-// 		}
-// 	};
-
-// 	const handleClearAddress = () => {
-// 		setAddress("");
-// 	};
-
-// 	// boolean 값으로 변경
-// 	const hasAddress = !!address;
-
-// 	return (
-// 		<>
-// 			<InputContainer>
-// 				<Input
-// 					placeholer="주소를 입력해주세요"
-// 					active={hasAddress}
-// 					value={address}
-// 					onChange={handleAddress}
-// 					onKeyDown={handleEnter}
-// 					onClick={onInputClick} // 검색 히스토리 보여줌
-// 				/>
-// 				{address && <RemoveIcon onClick={handleClearAddress}>❌</RemoveIcon>}
-// 			</InputContainer>
-// 		</>
-// 	);
-// }
-
-export default function CustomizedInputBase({ setAddress }) {
+export default function SearchBar({ setAddress }) {
 	const [zipcode, setZipCode] = useState("");
 	const [open, setOpen] = React.useState(false);
 	const handleOpen = () => setOpen(true);
--- src/components/SideHead.js
+++ src/components/SideHead.js
@@ -2,8 +2,7 @@
 
 import logo from "../assets/logo.png"
 import styled from "styled-components"
-// import AddressSearch from "./Search/AddressSearch.js";
-import CustomizedInputBase from "./Search/SearchBar.js"
+import SearchBar from "./Search/SearchBar.js"
 
 const Header = styled.div`
   display: flex;
@@ -29,8 +28,7 @@ function SideHead({ setAddress }) {
   return (
     <Header>
       <img src={logo} className="logo" alt="logo" />
-      {/* <AddressSearch /> */}
-      <CustomizedInputBase setAddress={setAddress} />
+      <SearchBar setAddress={setAddress} />
     </Header>
   )
 }
