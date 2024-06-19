08d75a2227d43463dca4bb6f0b41caf6451da316
nbko <nbko@uchicago.edu>
2024-06-19 17:31:28 +0900

    ccctv데이터 가져옴

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,13 +8,59 @@ import {
 	ZoomControl,
 	Circle,
 } from "react-kakao-maps-sdk";
+import axios from "axios";
 
 const { kakao } = window;
 
+// 백엔드에서 위도 경도를 소수점 포함 맥시멈 10글자로 제한함
+function formatCoordinate(value) {
+	return parseFloat(value.toFixed(6));
+}
+
+// cctv 데이터 가져오는 함수
+// http://kdt-ai-10-team01.elicecoding.com
+// http://kdt-ai-10-team01.elicecoding.com:3000/map/cctv?longitude=127.084032&latitude=37.483869&distance=1000
+async function fetchCCTVData(longitude, latitude, distance) {
+	const BASE_URL = "http://localhost:3000/"; // 기본 URL 설정
+
+	const baseInstance = await axios.create({
+		baseURL: BASE_URL, // 기본 URL 설정
+	});
+
+	const formattedLongitude = formatCoordinate(longitude);
+	const formattedLatitude = formatCoordinate(latitude);
+
+	// 9-10자리 limit (위도 경도)
+	try {
+		const response = await baseInstance.get(`/map/cctv`, {
+			params: {
+				longitude: formattedLongitude,
+				latitude: formattedLatitude,
+				distance: distance,
+			},
+		});
+		const cctvData = response.data;
+		// console.log("cctv data:", cctvData);
+		return cctvData;
+	} catch (error) {
+		console.log("보낸 데이터:", { longitude, latitude, distance });
+		console.log("cctv 데이터 가져오는데 실패:", error);
+	}
+}
+
+// 카카오 맵 함수
 export default function KakaoMap({ address }) {
+	// 현재 위치 (임시 저장)
+	const [centerLocation, setCenterLocation] = useState({
+		latitude: 37.483869,
+		longitude: 127.084032,
+	});
+
+	const [locations, setLocations] = useState([]);
+
 	useEffect(() => {
-		console.log("kakaomap given address:", address);
 		if (address) {
+			// 사용자가 입력한 주소를 위도 경도로 바꿔주는 함수 (유효한 주소일때만)
 			let geocoder = new kakao.maps.services.Geocoder();
 			geocoder.addressSearch(address, (result, status) => {
 				if (status === kakao.maps.services.Status.OK) {
@@ -22,54 +68,46 @@ export default function KakaoMap({ address }) {
 					const { x: longitude, y: latitude } = result[0];
 					setCenterLocation({ latitude, longitude });
 				} else {
-					console.error("Geocode was not successful: " + status);
+					console.error("Invalid Address: " + status);
 				}
 			});
 		}
-	}, [address]);
-
-	// 현재 위치
-	const [centerLocation, setCenterLocation] = useState({
-		latitude: 33.450701,
-		longitude: 126.570667,
-	});
-
-	const [locations, setLocations] = useState([]);
+	}, [address]); // 사용자가 입력한 주소가 바뀔 때마다 위도와 경도 받아와서 마커 위치 업데이트 하기
 
 	useEffect(() => {
-		const newLocations = [
-			{
-				title: "생태연못",
-				latlng: {
-					lat: centerLocation.latitude + 0.001,
-					lng: centerLocation.longitude + 0.0001,
-				},
-			},
-			{
-				title: "텃밭",
-				latlng: {
-					lat: centerLocation.latitude,
-					lng: centerLocation.longitude + 0.002,
-				},
-			},
-			{
-				title: "근린공원",
-				latlng: {
-					lat: centerLocation.latitude - 0.002,
-					lng: centerLocation.longitude,
-				},
-			},
-			{
-				title: "01강",
-				latlng: {
-					lat: centerLocation.latitude,
-					lng: centerLocation.longitude - 0.002,
-				},
-			},
-		];
+		// const newLocations = [
+		// 	{
+		// 		title: "생태연못",
+		// 		latlng: {
+		// 			lat: centerLocation.latitude + 0.001,
+		// 			lng: centerLocation.longitude + 0.0001,
+		// 		},
+		// 	},
+		// 	{
+		// 		title: "텃밭",
+		// 		latlng: {
+		// 			lat: centerLocation.latitude,
+		// 			lng: centerLocation.longitude + 0.002,
+		// 		},
+		// 	},
+		// 	{
+		// 		title: "근린공원",
+		// 		latlng: {
+		// 			lat: centerLocation.latitude - 0.002,
+		// 			lng: centerLocation.longitude,
+		// 		},
+		// 	},
+		// ];
 
-		setLocations(newLocations);
-	}, [centerLocation]);
+		fetchCCTVData(centerLocation.longitude, centerLocation.latitude, 500)
+			.then((data) => {
+				setLocations(data);
+				console.log("fetched cctv data:", data);
+			})
+			.catch((error) => {
+				console.log("failed to fetch cctv data:", error);
+			});
+	}, [centerLocation]); // 지도의 중심 위치가 바뀔때마다 중심 위치 기준으로 새로운 데이터 받아오기
 
 	return (
 		<>
@@ -101,17 +139,20 @@ export default function KakaoMap({ address }) {
 						fillColor={"#CFE7FF"} // 채우기
 						fillOpacity={0.3} // 채우기 불투명도
 					/>
-					{locations.map((loc, idx) => (
-						<MapMarker
-							key={`${loc.title}-${idx}`}
-							position={loc.latlng}
-							image={{
-								src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
-								size: { width: 24, height: 35 },
-							}}
-							title={loc.title}
-						/>
-					))}
+					{locations &&
+						locations.map((loc, idx) => (
+							<MapMarker
+								key={`${loc.latitude}-${idx}`}
+								position={{
+									lat: loc.latitude,
+									lng: loc.longitude,
+								}}
+								image={{
+									src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+									size: { width: 24, height: 35 },
+								}}
+							/>
+						))}
 					<MapTypeControl position={"TOPRIGHT"} />
 					<ZoomControl position={"RIGHT"} />
 				</Map>
--- src/components/SideHead.js
+++ src/components/SideHead.js
@@ -1,187 +1,184 @@
 /** @format */
 
-import * as React from "react"
-import { useState } from "react"
-import { Stack, Chip, Box } from "@mui/material"
-import logo from "../assets/logo.png"
-import styled from "styled-components"
-import SearchBar from "./Search/SearchBar.js"
-import Menu from "@mui/material/Menu"
-import MenuItem from "@mui/material/MenuItem"
-import DaumPostcode from "react-daum-postcode"
-import Modal from "@mui/material/Modal"
-import { styled as muiStyled } from "@mui/material/styles"
+import * as React from "react";
+import { useState } from "react";
+import { Stack, Chip, Box } from "@mui/material";
+import logo from "../assets/logo.png";
+import styled from "styled-components";
+import SearchBar from "./Search/SearchBar.js";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import DaumPostcode from "react-daum-postcode";
+import Modal from "@mui/material/Modal";
+import { styled as muiStyled } from "@mui/material/styles";
 
 const Header = styled.div`
-  background-color: #fff;
-`
+	background-color: #fff;
+`;
 
 const HeadInner = styled.div`
-  width: 90%;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+	width: 90%;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 
-  & img {
-    padding-top: 5vh;
-    padding-bottom: 2vh;
-    width: 90%;
-  }
+	& img {
+		padding-top: 5vh;
+		padding-bottom: 2vh;
+		width: 90%;
+	}
 
-  @media (max-width: 768px) {
-    align-items: flex-start;
-    & img {
-      padding-top: 3vh;
-      padding-bottom: 1vh;
-      padding-left: 8%;
-      width: 80%;
-    }
-  }
-`
+	@media (max-width: 768px) {
+		align-items: flex-start;
+		& img {
+			padding-top: 3vh;
+			padding-bottom: 1vh;
+			padding-left: 8%;
+			width: 80%;
+		}
+	}
+`;
 const Chips = styled.div`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 	width: 90%;
 	height: 5rem;
-`
+`;
 
 const StyledChip = muiStyled(Chip)(({ theme }) => ({
-  fontSize: "1.2rem",
-  fontWeight: 700,
-  height: "30px", // Chip 높이 조정
-  "& .MuiChip-label": {
-    padding: "0 10px", // 텍스트 패딩 조정
-  },
-}))
+	fontSize: "1.2rem",
+	fontWeight: 700,
+	height: "30px", // Chip 높이 조정
+	"& .MuiChip-label": {
+		padding: "0 10px", // 텍스트 패딩 조정
+	},
+}));
 
 function SideHead({ setAddress }) {
-  // const [open, setOpen] = useState(false);
-  const [searchHistory, setSearchHistory] = useState([])
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const [zipcode, setZipCode] = useState("")
-  const open = Boolean(anchorEl)
+	// const [open, setOpen] = useState(false);
+	const [searchHistory, setSearchHistory] = useState([]);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [zipcode, setZipCode] = useState("");
+	const open = Boolean(anchorEl);
+	const [openAddressSearch, setOpenAddressSearch] = useState(false);
+	const [clickedSearch, setClickedSearch] = useState("");
 
-  const [openAddressSearch, setOpenAddressSearch] = useState(false)
+	const closeAddressSearch = () => setOpenAddressSearch(false);
 
-  const closeAddressSearch = () => setOpenAddressSearch(false)
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
+	const style = {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%, -50%)",
+		width: 400,
+		bgcolor: "background.paper",
+		border: "2px solid #316BFF",
+		boxShadow: 24,
+		p: 4,
+	};
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #316BFF",
-    boxShadow: 24,
-    p: 4,
-  }
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+	const searchHistoryHandleClick = (event) => {
+		console.log(event.currentTarget);
+		setClickedSearch(event.currentTarget);
+		setAnchorEl(null);
+		// setOpenAddressSearch(true);
+	};
 
-  const searchHistoryHandleClose = () => {
-    setAnchorEl(null)
-    setOpenAddressSearch(true)
-  }
+	const handleAddress = (data) => {
+		const { address, zonecode } = data;
+		setZipCode(zonecode);
+		setAddress(address);
+		setSearchHistory((searchHis) => [...searchHis, address]);
+		handleClose();
+	};
 
-  const handleAddress = (data) => {
-    const { address, zonecode } = data
-    setZipCode(zonecode)
-    setAddress(address)
-    setSearchHistory((searchHis) => [...searchHis, address])
-    // console.log("address", address);
-    // console.log("zipcode:", zonecode);
-    handleClose()
-  }
-
-  return (
-    <Header>
-      <HeadInner>
-        <img src={logo} className="logo" alt="logo" />
-        <SearchBar
-          setAddress={setAddress}
-          setSearchHistory={setSearchHistory}
-        />
-        <Chips>
-          <Stack direction="row" spacing={1}>
-            <StyledChip
-              label="CCTV"
-              color="primary"
-              variant="outlined"
-              onClick={handleClick}
-            />
-            <StyledChip
-              label="보안등"
-              color="primary"
-              variant="outlined"
-              onClick={handleClick}
-            />
-            <StyledChip
-              label="거리"
-              color="primary"
-              variant="outlined"
-              onClick={handleClick}
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={1}
-          >
-            <StyledChip
-              label="검색 히스토리"
-              onClick={handleClick}
-              sx={{
-                color: "#fff",
-                backgroundColor: "#1976d2",
-                borderRadius: "2rem",
-              }}
-            />
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              {searchHistory.length > 0 ? (
-                searchHistory.map((children, search) => {
-                  return (
-                    <MenuItem
-                      className="item_place"
-                      style={{ fontSize: "1.3rem" }}
-                      aria-label="최근검색"
-                      onClick={searchHistoryHandleClose}
-                    >
-                      {children}
-                    </MenuItem>
-                  )
-                })
-              ) : (
-                <MenuItem onClick={handleClose}>
-                  검색한 기록이 없습니다
-                </MenuItem>
-              )}
-            </Menu>
-          </Stack>
-        </Chips>
-        <Modal open={openAddressSearch} onClose={closeAddressSearch}>
+	return (
+		<Header>
+			<HeadInner>
+				<img src={logo} className="logo" alt="logo" />
+				<SearchBar
+					setAddress={setAddress}
+					setSearchHistory={setSearchHistory}
+				/>
+				<Chips>
+					<Stack direction="row" spacing={1}>
+						<StyledChip
+							label="CCTV"
+							color="primary"
+							variant="outlined"
+							onClick={handleClick}
+						/>
+						<StyledChip
+							label="보안등"
+							color="primary"
+							variant="outlined"
+							onClick={handleClick}
+						/>
+						<StyledChip
+							label="거리"
+							color="primary"
+							variant="outlined"
+							onClick={handleClick}
+						/>
+					</Stack>
+					<Stack direction="row" spacing={1}>
+						<StyledChip
+							label="검색 히스토리"
+							onClick={handleClick}
+							sx={{
+								color: "#fff",
+								backgroundColor: "#1976d2",
+								borderRadius: "2rem",
+							}}
+						/>
+						<Menu
+							id="basic-menu"
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							MenuListProps={{
+								"aria-labelledby": "basic-button",
+							}}
+						>
+							{searchHistory.length > 0 ? (
+								searchHistory.map((children, search) => {
+									return (
+										<MenuItem
+											className="item_place"
+											style={{ fontSize: "1.3rem" }}
+											aria-label="최근검색"
+											onClick={searchHistoryHandleClick}
+										>
+											{children}
+										</MenuItem>
+									);
+								})
+							) : (
+								<MenuItem onClick={handleClose}>
+									검색한 기록이 없습니다
+								</MenuItem>
+							)}
+						</Menu>
+					</Stack>
+				</Chips>
+				{/* <Modal open={openAddressSearch} onClose={closeAddressSearch}>
           <Box sx={style}>
             <DaumPostcode onComplete={handleAddress} />
           </Box>
-        </Modal>
-      </HeadInner>
-    </Header>
-  )
+        </Modal> */}
+			</HeadInner>
+		</Header>
+	);
 }
 
-export default SideHead
+export default SideHead;
